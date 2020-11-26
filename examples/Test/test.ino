e2fc327ea4516df38916b35bab734bb4b57cfc07
Mehmet Günce Akkoyun <akkoyun@me.com>
2020-11-26 15:06:48 +0300

    Initial Update

--- examples/Test/test.ino
+++ examples/Test/test.ino
@@ -1,10 +1,30 @@
+// Define Ovoo Libraries
+#include "GE910.h"			// IoT Library
+
+// Define Object
+GE910 GSM;
 
 void setup() {
-  // put your setup code here, to run once:
+ 
+ 	// Activate IoT Modem
+	GSM.Module_ON();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+	/ Control for Connection
+	if (GSM.Connected == true and GSM.CREG == 1 and GSM.CGREG == 1) {
+		
+		// Update GSM RTC
+		GSM.Time_Update();
+		
+		// Control for HTTPConfig
+		if (GSM.AT_HTTPCFG() == true) {
+
+
+		}
+		
+	}
+	
 }
