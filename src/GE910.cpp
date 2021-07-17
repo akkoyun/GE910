d2e6c4a0dfd7c1d38ea26e184532e8b7b47fea81
Mehmet Günce Akkoyun <akkoyun@me.com>
2021-07-17 20:17:02 +0300

    Update GE910.cpp

--- src/GE910.cpp
+++ src/GE910.cpp
@@ -87,7 +87,7 @@ bool GE910::AT(void) {
 		} else {
 
 			// Set Response Variable
-			strcpy(Command_Response, (char)NOT_COMMAND);
+			strcpy(Command_Response, "1");
 
 			// Set Control Variable
 			Control.AT = false;
