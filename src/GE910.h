c3392f328d69a0e3fe43a89230363beb4e8865e7
Mehmet Günce Akkoyun <akkoyun@me.com>
2021-07-17 19:41:09 +0300

    01.17.01 Update

--- src/GE910.h
+++ src/GE910.h
@@ -25,7 +25,7 @@ public:
 	// ************************************************************
 
 	// Function Variables
-	char 					Version[9] 				= "01.16.01";		// Version Variable
+	char 					Version[9] 				= "01.17.01";		// Version Variable
 
 	// IoT Variables
 	char 					ID[11]					= "";				// ID Variable
@@ -87,78 +87,8 @@ public:
 		TIME_OUT			// 2
 	};
 
-	// ************************************************************
-	// Public Functions
-	// ************************************************************
-	
-	// Hardware Functions
-	void LED(bool State);												// GSM LED Inducator Power En Function
-
-	// Batch Functions
-	bool Time_Update(void);												// GSM Embedded RTC Time Update Function
-	bool Connection_Control(void);										// Connection Control Function
-	void Connect(void);													// Connection Function
-	bool Send(const String &_Data);										// Data Send Function
-	bool RSSI_Refresh(void);											// RSSI Refresh Function
-	void Listen(void);													// Listen UART
-	void Socket_Get_Command(void);
-	bool Activate(bool Status);
-
-private:
-	
-	// ************************************************************
-	// Private Functions
-	// ************************************************************
-
-	// Hardware Functions
-	void Communication(bool State);
-	void Power(bool State);
-	void OnOff(int Time_);
-	void ShutDown(int Time_);
-	bool PowerMonitor(void);
-
-	// Modem AT Command Functions
-	bool AT(void);
-	bool AT_CMEE(uint8_t _CMEE);
-	bool AT_FCLASS(uint8_t _FCLASS);
-	bool AT_K(uint8_t _K);
-	bool AT_CPIN(void);
-	bool AT_CCID(void);
-	bool AT_CGSN(void);
-	bool AT_GSN(void);
-	bool AT_SLED(uint8_t _SLED);
-	bool AT_TXMONMODE(uint8_t _TXMONMODE);
-	bool AT_REGMODE(uint8_t _REGMODE);
-	bool AT_CREG(void);
-	bool AT_CGREG(void);
-	bool AT_CGDCONT(char *_PDP, char *_APN);
-	bool AT_SCFG(uint8_t _ConnID, uint8_t _PktSz, uint8_t _MaxTo, uint8_t _ConnTo, uint8_t _TxTo);
-	bool AT_SGACT(void);
-	bool AT_CSQ(void);
-	bool AT_SERVINFO(void);
-	bool AT_CCLK(void);
-	bool AT_NTP(char *_NTP_Server);
-	bool AT_CTZU(uint8_t _Ctzu);
-	bool AT_HTTPCFG(char *_HTTP_Server, uint8_t _Port);
-	bool AT_HTTPSND(char *_URL, const String &Data);
-	bool AT_SL(uint8_t _ConnID, bool _State, uint8_t _Port);
-	bool AT_FRWL(const String &_IP);
-	bool AT_SA(void);
-	bool AT_SH(void);
-	bool AT_SHDN(void);
-	bool AT_SRECV(void);
-	bool AT_SCFGEXT(uint8_t _srMode, uint8_t _recvDataMode, uint8_t _keepalive);
-	bool AT_E2SLRI(void);
-	
-	// Batch Functions
-	bool Connection_AT_Batch(void);
-	bool Recieve_AT_Batch(void);
-		
-	// Utility Functions
-	bool Response_Wait(uint16_t _Length, uint16_t _TimeOut);
-
 	// ************************************************************************************************************************
-	// Private GSM Setting Variables
+	// Public GSM Setting Variables
 	// ************************************************************************************************************************
 	struct Parameter_Struct {
 		//------------------------------------------------------------------
@@ -206,10 +136,6 @@ private:
 		0,
 		1
 	};
-
-	// ************************************************************************************************************************
-	// Private GSM Command Control Variables
-	// ************************************************************************************************************************
 	struct Control_Struct {
 
 		//------------------------------------------------------------------
@@ -250,7 +176,74 @@ private:
 	}; Control_Struct Control {
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 	};
-	// ************************************************************************************************************************
+
+	// ************************************************************
+	// Public Functions
+	// ************************************************************
+	
+	// Hardware Functions
+	void LED(bool State);
+	void Communication(bool State);
+	void Power(bool State);
+	void OnOff(int Time_);
+	void ShutDown(int Time_);
+	bool PowerMonitor(void);
+
+	// Modem AT Command Functions
+	bool AT(void);
+	bool AT_CMEE(uint8_t _CMEE);
+	bool AT_FCLASS(uint8_t _FCLASS);
+	bool AT_K(uint8_t _K);
+	bool AT_CPIN(void);
+	bool AT_CCID(void);
+	bool AT_CGSN(void);
+	bool AT_GSN(void);
+	bool AT_SLED(uint8_t _SLED);
+	bool AT_TXMONMODE(uint8_t _TXMONMODE);
+	bool AT_REGMODE(uint8_t _REGMODE);
+	bool AT_CREG(void);
+	bool AT_CGREG(void);
+	bool AT_CGDCONT(char *_PDP, char *_APN);
+	bool AT_SCFG(uint8_t _ConnID, uint8_t _PktSz, uint8_t _MaxTo, uint8_t _ConnTo, uint8_t _TxTo);
+	bool AT_SGACT(void);
+	bool AT_CSQ(void);
+	bool AT_SERVINFO(void);
+	bool AT_CCLK(void);
+	bool AT_NTP(char *_NTP_Server);
+	bool AT_CTZU(uint8_t _Ctzu);
+	bool AT_HTTPCFG(char *_HTTP_Server, uint8_t _Port);
+	bool AT_HTTPSND(char *_URL, const String &Data);
+	bool AT_SL(uint8_t _ConnID, bool _State, uint8_t _Port);
+	bool AT_FRWL(const String &_IP);
+	bool AT_SA(void);
+	bool AT_SH(void);
+	bool AT_SHDN(void);
+	bool AT_SRECV(void);
+	bool AT_SCFGEXT(uint8_t _srMode, uint8_t _recvDataMode, uint8_t _keepalive);
+	bool AT_E2SLRI(void);
+	
+	// Batch Functions
+	bool Time_Update(void);												// GSM Embedded RTC Time Update Function
+	bool Connection_Control(void);										// Connection Control Function
+	void Connect(void);													// Connection Function
+	bool Send(const String &_Data);										// Data Send Function
+	bool RSSI_Refresh(void);											// RSSI Refresh Function
+	void Listen(void);													// Listen UART
+	void Socket_Get_Command(void);
+	bool Activate(bool Status);
+
+private:
+	
+	// ************************************************************
+	// Private Functions
+	// ************************************************************
+	
+	// Batch Functions
+	bool Connection_AT_Batch(void);
+	bool Recieve_AT_Batch(void);
+		
+	// Utility Functions
+	bool Response_Wait(uint16_t _Length, uint16_t _TimeOut);
 
 };
 
